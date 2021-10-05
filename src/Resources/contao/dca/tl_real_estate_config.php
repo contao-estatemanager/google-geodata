99252cffa401eb1c026786e114613515d9116d27
doishub <daniele@oveleon.de>
2021-10-05 10:05:17 +0200

    CS

--- src/Resources/contao/dca/tl_real_estate_config.php
+++ src/Resources/contao/dca/tl_real_estate_config.php
@@ -1,25 +1,31 @@
 <?php
-/**
+
+declare(strict_types=1);
+
+/*
  * This file is part of Contao EstateManager.
  *
- * @link      https://www.contao-estatemanager.com/
- * @source    https://github.com/contao-estatemanager
- * @copyright Copyright (c) 2019  Oveleon GbR (https://www.oveleon.de)
- * @license   https://www.contao-estatemanager.com/lizenzbedingungen.html
+ * @see        https://www.contao-estatemanager.com/
+ * @source     https://github.com/contao-estatemanager/google-geodata
+ * @copyright  Copyright (c) 2021 Oveleon GbR (https://www.oveleon.de)
+ * @license    https://www.contao-estatemanager.com/lizenzbedingungen.html
  */
 
-if(ContaoEstateManager\GoogleGeodata\AddonManager::valid()) {
+use Contao\CoreBundle\DataContainer\PaletteManipulator;
+use ContaoEstateManager\GoogleGeodata\AddonManager;
+
+if (AddonManager::valid())
+{
     // Add field
-    $GLOBALS['TL_DCA']['tl_real_estate_config']['fields']['storeGeoData'] = array
-    (
-        'label'                   => &$GLOBALS['TL_LANG']['tl_real_estate_config']['storeGeoData'],
-        'inputType'               => 'checkbox',
-        'eval'                    => array('tl_class'=>'w50 m12')
-    );
+    $GLOBALS['TL_DCA']['tl_real_estate_config']['fields']['storeGeoData'] = [
+        'label' => &$GLOBALS['TL_LANG']['tl_real_estate_config']['storeGeoData'],
+        'inputType' => 'checkbox',
+        'eval' => ['tl_class' => 'w50 m12'],
+    ];
 
     // Extend default palette
-    Contao\CoreBundle\DataContainer\PaletteManipulator::create()
-        ->addField(array('storeGeoData'), 'google_legend', Contao\CoreBundle\DataContainer\PaletteManipulator::POSITION_APPEND)
+    PaletteManipulator::create()
+        ->addField(['storeGeoData'], 'google_legend', PaletteManipulator::POSITION_APPEND)
         ->applyToPalette('default', 'tl_real_estate_config')
     ;
 }
